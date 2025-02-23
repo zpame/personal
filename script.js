6612357fab731f20dabfc9d31141fdde1fd8d3b1
zpame <zacharypamer@hotmail.com>
2025-02-23 15:58:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+window.addEventListener("load", function () {
+    let cssLink = document.querySelector("link[rel='stylesheet']");
+    if (cssLink) {
+        cssLink.href = cssLink.href.split("?")[0] + "?v=" + new Date().getTime();
+    }
+});
+
