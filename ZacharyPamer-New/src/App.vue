c45f376add7747a36592af3e9c45f4c90787591c
zpame <zacharypamer@hotmail.com>
2026-02-01 21:37:51 -0500

    First new commit.

---
+++ ZacharyPamer-New/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import SeniorPic from '@/assets/seniorpic2.jpg'
+</script>
+
+<template>
+  <div id = "Img1">
+    <img :src="SeniorPic" alt="Profile Picture">
+    </div>
+    <h1>Zachary Pamer</h1>
+    <div id="projects">
+        <h1><a href="projects.html">Projects</a></h1>
+    </div>
+    <div id="hobbies">
+        <h1><a href="Hobbies.html">Hobbies</a></h1>
+    </div>
+
+</template>
