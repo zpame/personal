97f7d1ae8b7ce911007ded57ffc000652d3091e4
zpame <zacharypamer@hotmail.com>
2026-02-09 22:48:19 -0500

    working on setup main page

--- ZacharyPamer-New/src/App.vue
+++ ZacharyPamer-New/src/App.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink } from 'vue-router'
 import SeniorPic from '@/assets/seniorpic1.jpg'
 import '../../style.css'
-
 </script>
 
 <template>
   <div class="Img1">
-    <img :src="SeniorPic" alt="Profile Picture">
-    </div>
-    <h1>Zachary Pamer</h1>
-    <div class="MainButton">
-        <h1><a href="projects.html">Projects</a></h1>
-    </div>
-    <div class="MainButton">
-        <h1><a href="Hobbies.html">Hobbies</a></h1>
-    </div>
+    <img :src="SeniorPic" alt="Profile Picture" />
+  </div>
+  <h1>Zachary Pamer</h1>
+  <RouterLink class="MainButton" to="/projects">Projects</RouterLink>
+  <br />
+  <RouterLink class="MainButton" to="/hobbies">Hobbies</RouterLink>
 
+  <RouterView />
 </template>
---
+++ ZacharyPamer-New/src/Projects.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+import '../../style.css'
+</script>
+
+<template>TBD</template>
